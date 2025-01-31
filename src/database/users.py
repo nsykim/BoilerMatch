d95dad70228a2c0e1f7d41742eda44188af7c71c
nsykim <noahsykim@gmail.com>
2025-01-31 15:57:44 -0500

    added try except block for error catching

--- src/database/users.py
+++ src/database/users.py
@@ -1,6 +1,15 @@
 from pymongo.collection import Collection
+import logging
 
 # Returns true an entry (match) within the table with provided field already exists
 def search_user_db(collection: Collection, field: str, match: str) -> bool:
-    return collection.find_one({field: match})
+    try:
+        ret = collection.find_one({field: match})
+        logging.info(f"search_user_db: found {ret}")
+        return True, ret
+    except:
+        logging.critical("search_user_db: could not search for the user")
+        return False, 0 
+
+
 
